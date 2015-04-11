feb4b852364dffe3df3e024eb62b38332ad3f8d2
Ubuntu <ubuntu@ip-10-0-115-231.ec2.internal>
2015-04-11 18:45:37 +0000

    hello

--- naive.py
+++ naive.py
@@ -25,11 +25,10 @@ def penny(market, stock, info):
         return
     penny_buy = info['bid']+1
     penny_sell = info['ask']-1
-    if (penny_buy - penny_sell) > 0:
-        market.buy_order(stock, penny_buy, 1)
-        # Check if we have stock
-        if 100 <= info['position']:
-            market.sell_order(stock, penny_sell, 1)
+    market.buy_order(stock, penny_buy, 1)
+   # Check if we have stock
+    if 100 <= info['position']:
+        market.sell_order(stock, penny_sell, 1)
         return
     else:
         return
@@ -49,7 +48,7 @@ def next_action(market):
     
     for stock, info in market.stocks.items():
         if market.num_orders() < 10:
-            FV_attempt(market, stock, info)
+            penny(market, stock, info)
         
         
         
