172ed493fef5051a3960118e0bbef7c67cb37f7a
skoppula <skoppula@mit.edu>
2015-04-11 11:50:07 -0400

    update util

--- util.py
+++ util.py
@@ -9,8 +9,16 @@ EX_PUBLIC_IP='52.5.76.103' #looking at observer
 EX_PRIVATE_IP='10.0.37.99' #connecting from bot box
 
 MARKET_INDEX = 0 #0 for slow, 1 for real, 2 for empty
-#JSON_PORT = 25000+index
-#PLAIN_TEXT_PORT = 20000+index
+JSON_PORT = 25000+MARKET_INDEX
+PLAIN_TEXT_PORT = 20000+MARKET_INDEX
+
+def main():
+    s = setup_connection()  
+    print get_message(s)
+    print send_hello(s)
+
+def send_json(sckt, msg):
+    sckt.send(json.dumps(msg))
 
 def setup_connection():
     try:
@@ -23,24 +31,20 @@ def setup_connection():
         print 'COULD NOT CONNECT'
         return None
 
-def send_json(sckt, msg):
-    sckt.send(json.dumps(msg))
-
-def listen_for_message(sckt):
-    infile = sckt.makefile()
-    while True:
+def get_message(sckt):
+    try:
+        infile = sckt.makefile()
         line = infile.readline()
-        #json.loads(s.recv(1024))
-        print json.loads(line)
+        return json.loads(line) 
+    except:
+        print 'OH NO COULD NOT PARSE JSON OR READ FROM SOCKET'
+        return None
 
-s = setup_connection()
-listen_for_message(s)
-
-def hello(sckt):
+def send_hello(sckt):
     hello_msg = {"type": "hello", "team": "SEA"}
+    send_json(s, hello_msg)
+    mkt_state = get_message(s)
+    return mkt_state
     
-def sell(sckt, order_id, symbol_id, price, size):
-    pass
-
-#def buy(sckt, order_id, symbol_id, price, size):
-
+if __name__ == '__main__':
+    main()
