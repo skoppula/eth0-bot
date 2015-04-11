0ef171629a093c8b303b87dd548b257c20980c63
skoppula <skoppula@mit.edu>
2015-04-11 10:37:43 -0400

    hello

---
+++ util.py
@@ -0,0 +1,19 @@
+import socket
+import sys
+
+TEAM_NAME='SEA'
+BOT_BOX_IP='52.4.171.0'
+PASS='EcAyY836zTdcFnAh'
+TEST_EX_PUBLIC_IP='52.5.76.103'
+TEST_EX_PRIVATE_IP='
+
+def send_json():
+
+
+def hello():
+    global TEAM_NAME
+    
+def sell():
+
+def buy(
+
